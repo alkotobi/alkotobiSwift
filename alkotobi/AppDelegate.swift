afd18417a5f5673c06b44126a696657092f19e4f
MERHAB NOUREDDINE <merhab@me.com>
2018-10-02 11:14:56 +0100

    Initial Commit

---
+++ alkotobi/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  alkotobi
+//
+//  Created by merhab on 2‏/10‏/2018.
+//  Copyright © 2018 merhab. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ alkotobi/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  alkotobi
+//
+//  Created by merhab on 2‏/10‏/2018.
+//  Copyright © 2018 merhab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
